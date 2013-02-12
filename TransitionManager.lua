8d0d4db5db9c9356a498b7c9e4802b606bc6f803
Stephann Vasconcelos <stephannvasconcelos@yahoo.com>
2013-02-12 10:13:49 -0300

    Update for best performance
    changed table.insert(myTable, value) to myTable[ #myTable + 1 ] = value

--- TransitionManager.lua
+++ TransitionManager.lua
@@ -1,24 +1,24 @@
 local TransitionManager = {}
 
---TRANSITION MANAGER CLASS
 TransitionManager.new = function()
     local transitionManager = {}
+    
     transitionManager.transitions = {}
     
     function transitionManager:to(object, param)
         local transition = transition.to(object, param)
         transition.isPaused = false
-        table.insert(self.transitions, transition)
+        self.transitions[#self.transitions + 1] = transition
     end
 
     function transitionManager:from(object, param)
         local transition = transition.from(object, param)
         transition.isPaused = false
-        table.insert(self.transitions, transition)
+        self.transitions[#self.transitions + 1] = transition
     end
 
     function transitionManager:update()
-        if table.maxn(self.transitions) > 0 then          
+        if #self.transitions > 0 then          
             for index, value in pairs(self.transitions) do
                 if (value._timeStart + value._duration < system.getTimer() and value.isPaused == false) then
                     table.remove(self.transitions, index)
@@ -29,7 +29,7 @@ TransitionManager.new = function()
     end
 
     function transitionManager:pause()
-        if table.maxn(self.transitions) > 0 then
+        if #self.transitions > 0 then
             for index, value in pairs(self.transitions) do
                 value.isPaused = true
                 value.timePause = system.getTimer()
@@ -39,12 +39,13 @@ TransitionManager.new = function()
     end
 
     function transitionManager:resume()
-        if table.maxn(self.transitions) > 0 then
+        if #self.transitions > 0 then
             for index, value in pairs(self.transitions) do
-                print(value._target.name)
+                local target = value._target
                 local param = value._keysFinish
                 param.time = value._duration - (value.timePause - value._timeStart)
-                local transition = transition.to(value._target, param)
+                value = nil
+                local transition = transition.to(target, param)
                 transition.isPaused = false
                 self.transitions[index] = transition
             end
@@ -53,3 +54,5 @@ TransitionManager.new = function()
 
     return transitionManager
 end
+
+return TransitionManager
